2939c30c94e442299a803e83dce3ef0065d69522
Vadim Mazalov <vadimma@microsoft.com>
2017-04-13 10:51:34 -0700

    Some script updates

---
+++ compress_scp_mlf_keys.py
@@ -0,0 +1,85 @@
+import os
+import argparse
+
+def adaptScp(scpFile, output):
+    result = {}
+    counter = 0
+    with open(output, "w") as outputFile:
+        with open(scpFile) as f:
+            for line in f:
+                splitted = line.split("=")
+                if len(splitted) != 2:
+                    raise Exception("Invalid input '%s', line with invalid format '%s'" % (scpFile, line))
+                key = splitted[0].strip()
+                if key.endswith(".mfc"):
+                    key = key[:-4]
+                newKey = str(counter)
+                key = key.replace('/', '\\')
+                result[key] = newKey
+                outputLine = newKey + "=" + splitted[1]
+                outputFile.write(outputLine)
+                counter += 1
+    return result
+
+def adaptLattice(latticeFile, output, keys):
+    counter = 0
+    with open(output, "w") as outputFile:
+        with open(latticeFile) as f:
+            for line in f:
+                splitted = line.split("=")
+                if len(splitted) != 2:
+                    raise Exception("Invalid input '%s', line with invalid format '%s'" % (latticeFile, line))
+                key = splitted[0].strip()
+                if key.endswith(".mfc"):
+                    key = key[:-4]
+                key = key.replace('/', '\\')
+                if key in keys:
+                    outputLine=keys[key] + "=" + splitted[1]
+                    outputFile.write(outputLine)
+                else:
+                    outputFile.write(outputLine)
+                    print("Unknown lattice key '%s'" % key)
+
+def adaptMlf(mlfFile, output, keys):
+    previous = ""
+    with open(output, "w") as outputFile:
+        with open(mlfFile) as inputFile:
+            for originalLine in inputFile:
+                line = originalLine.strip()
+                if previous == "." or previous == "#!MLF!#":
+                    line = line.strip('"')
+                    if line.endswith(".lab"):
+                        line = line[:-4]
+                    line = line.replace('/', '\\')
+                    if line in keys:
+                        line = '"' + keys[line] + '"' + "\n"
+                        outputFile.write(line)
+                    else:
+                        outputFile.write(originalLine)
+                else:
+                    outputFile.write(originalLine)
+                previous = line.strip()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Adapts mlf and scp files to use logical keys of smaller length")
+    parser.add_argument('--inputScpFile', help='Input scp file.', required=True)
+    parser.add_argument('--inputMlfFile', help='Input mlf file that corresponds to the scp file.', required=True)
+    parser.add_argument('--inputLatticeFile', help='Input lattice file.', required=False)
+    parser.add_argument('--outputScpFile', help='Output scp file.', required=True)
+    parser.add_argument('--outputMlfFile', help='Output mlf file.', required=True)
+    parser.add_argument('--outputLatticeFile', help='Output mlf file.', required=False)
+    args = parser.parse_args()
+    
+    if not os.path.exists(args.inputScpFile):
+        print('Input scp file is invalid.')
+
+    if not os.path.exists(args.inputMlfFile):
+        print('Input mlf file is invalid.')
+
+    if args.inputLatticeFile and not os.path.exists(args.inputLatticeFile):
+        print('Input lattice file is invalid')
+
+    keys = adaptScp(args.inputScpFile, args.outputScpFile)
+    adaptMlf(args.inputMlfFile, args.outputMlfFile, keys)
+    if args.inputLatticeFile:
+        adaptLattice(args.inputLatticeFile, args.outputLatticeFile, keys)
---
+++ grl_domain_prepapre.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+
+def adaptScp(scpFile):
+    result = {}
+    counter = 0
+    with open(scpFile) as f:
+        for line in f:
+            splitted = line.split("=")
+            if len(splitted) != 2:
+                raise Exception("Invalid input '%s', line with invalid format '%s'" % (scpFile, line))
+            key = splitted[0].strip()
+            if key.endswith(".mfc"):
+                key = key[:-4]
+            newKey = str(counter)
+            key.replace('/', '\\');
+            result[key] = newKey
+            counter += 1
+    return result
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extracts domain labels from key-value pair file, phone list file and the MLF file")
+    parser.add_argument('--inputScpFile', help='Input scp file.', required=True)
+    parser.add_argument('--outputDomainLabelFile', help='Input scp file.', required=True)
+    args = parser.parse_args()
+
+    if not os.path.exists(args.inputScpFile):
+        print('Input scp file is invalid.')
+
+    keys = adaptScp(args.inputScpFile)
+
+    with open(args.outputDomainLabelFile, "w") as outputFile:
+        with open(args.inputScpFile) as f:
+            for line in f:
+                splitted = line.split("=")
+                if len(splitted) != 2:
+                    raise Exception("Invalid input '%s', line with invalid format '%s'" % (scpFile, line))
+                key = splitted[0].strip()
+                if key.endswith(".mfc"):
+                    key = key[:-4]
+
+                frameBound = splitted[1].split("[")[1].split("]")[0].split(",")
+                if len(frameBound) != 2:
+                    raise Exception("Invalid frame bound '%s', line with invalid format '%s'" % (scpFile, line))
+
+                label = "1" if key.endswith("_n") else "0"
+                for i in range(int(frameBound[0]), int(frameBound[1])):
+                   outputFile.write(keys[key] + " |l " + label)
